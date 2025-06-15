4e53bfb6a930786b8372df577c5b08e214736500
alona <alenabask93@gmail.com>
2025-06-15 14:23:37 +0200

    update

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { type Note, type NoteTag, type NotesResponse } from '../types/note';
 
 const API_BASE = 'https://notehub-public.goit.study/api';
-
 const token = import.meta.env.VITE_NOTEHUB_TOKEN;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE,
   headers: {
-    Authorization: `Bearer ${token}`, 
+    Authorization: `Bearer ${token}`,
   },
 });
+
 
 export interface FetchNotesParams {
   page?: number;
@@ -18,20 +18,31 @@ export interface FetchNotesParams {
   search?: string;
 }
 
+
 export interface FetchNotesResponse {
   notes: Note[];
   totalPages: number;
   totalNotes: number;
 }
 
-export async function fetchNotes({ page, perPage, search }: { page: number, perPage: number, search: string }): Promise<NotesResponse> {
-  const params: Record<string, unknown> = {
+
+export async function fetchNotes({
+  page = 1,
+  perPage = 12,
+  search = '',
+}: FetchNotesParams): Promise<NotesResponse> {
+  const params: Record<string, string | number> = {
     page,
-    limit: perPage,
-    search: search || '', 
+    perPage,
   };
 
-  const { data } = await axiosInstance.get<FetchNotesResponse>('/notes', { params });
+  if (search.trim()) {
+    params.search = search.trim();
+  }
+
+  const { data } = await axiosInstance.get<FetchNotesResponse>('/notes', {
+    params,
+  });
 
   return {
     notes: data.notes,
@@ -39,22 +50,26 @@ export async function fetchNotes({ page, perPage, search }: { page: number, perP
   };
 }
 
+
 export interface CreateNoteParams {
   title: string;
   content?: string;
   tag: NoteTag;
 }
 
+
 export async function createNote(params: CreateNoteParams): Promise<Note> {
   const { data } = await axiosInstance.post<Note>('/notes', params);
   return data;
 }
 
+// Відповідь при видаленні нотатки
 export interface DeleteNoteResponse {
   message: string;
   note: Note;
 }
 
+// Видалення нотатки
 export async function deleteNote(id: string): Promise<DeleteNoteResponse> {
   const { data } = await axiosInstance.delete<DeleteNoteResponse>(`/notes/${id}`);
   return data;
--- src/types/note.ts
+++ src/types/note.ts
@@ -1,20 +1,15 @@
 export interface Note {
-    _id: string;
-    title: string;
-    content: string;
-    tag: NoteTag;
-    createdAt: string;
-    updatedAt: string;
-}
-export interface FormValues {
+  _id: string;
   title: string;
-  content: string;
+  content?: string;
   tag: NoteTag;
+  createdAt: string;
+  updatedAt: string;
 }
-  
+
 export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
-  
+
 export interface NotesResponse {
-    notes: Note[];
-    totalPages: number;
-  }
+  notes: Note[];
+  totalPages: number;
+}
